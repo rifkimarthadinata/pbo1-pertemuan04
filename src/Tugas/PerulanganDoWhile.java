e22b022acfa38880671ecb95dad73b910bb9e462
rifkimarthadinata <62433657+rifkimarthadinata@users.noreply.github.com>
2020-04-25 14:05:00 +0800

    1 class tambahan PerulanganDoWhile

---
+++ src/Tugas/PerulanganDoWhile.java
@@ -0,0 +1,14 @@
+package Tugas;
+
+public class PerulanganDoWhile {
+    public static void main(String[] args) {
+
+        // membuat variabel
+        int i = 0;
+
+        do {
+            System.out.println("perulangan ke-" + i);
+            i++;
+        }while (i <= 25);
+    }
+}
