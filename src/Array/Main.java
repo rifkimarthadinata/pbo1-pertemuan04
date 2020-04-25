12407b2f58ded638ab2cde749b6731423012aeff
rifkimarthadinata <62433657+rifkimarthadinata@users.noreply.github.com>
2020-04-25 13:17:04 +0800

    Array2 apabila saya tambahkan sout maka akan timbul hasilnya

--- src/Array/Main.java
+++ src/Array/Main.java
@@ -2,9 +2,19 @@ package Array;
 
 public class Main {
     public static void main(String[] args) {
-        int nilaiArr[] = {40,55,63,17,22,68,89,97,89};
-        System.out.println(nilaiArr.length);
-        System.out.println(nilaiArr[0]);
-        System.out.println(nilaiArr[8]);
+        String mataKuliah[];
+        mataKuliah = new String[10];
+
+        mataKuliah[0] = "Pemrograman Berbasis Objek 1";
+        mataKuliah[1] = "Visual 2";
+        mataKuliah[2] = "Rekayasa Perangkat Lunak";
+        mataKuliah[3] = "Organisasi Komputer";
+        mataKuliah[4] = "Pemrograman Web 2";
+
+        System.out.println(mataKuliah[0]);
+        System.out.println(mataKuliah[1]);
+        System.out.println(mataKuliah[2]);
+        System.out.println(mataKuliah[3]);
+        System.out.println(mataKuliah[4]);
     }
 }
