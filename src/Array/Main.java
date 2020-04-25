2646e76da973230dca58d3ed997755bf9a178d77
rifkimarthadinata <62433657+rifkimarthadinata@users.noreply.github.com>
2020-04-25 13:36:30 +0800

    multi dimensional array

--- src/Array/Main.java
+++ src/Array/Main.java
@@ -2,19 +2,37 @@ package Array;
 
 public class Main {
     public static void main(String[] args) {
-        String mataKuliah[];
-        mataKuliah = new String[10];
+        double[][] nilaiMahasiswa = new double[5][30];
+        double bobot[] = {0.1, 0.2, 0.3, 0.4};
 
-        mataKuliah[0] = "Pemrograman Berbasis Objek 1";
-        mataKuliah[1] = "Visual 2";
-        mataKuliah[2] = "Rekayasa Perangkat Lunak";
-        mataKuliah[3] = "Organisasi Komputer";
-        mataKuliah[4] = "Pemrograman Web 2";
+        nilaiMahasiswa[0][0] = 80;
+        nilaiMahasiswa[1][0] = 90;
+        nilaiMahasiswa[2][0] = 75;
+        nilaiMahasiswa[3][0] = 70;
 
-        System.out.println(mataKuliah[0]);
-        System.out.println(mataKuliah[1]);
-        System.out.println(mataKuliah[2]);
-        System.out.println(mataKuliah[3]);
-        System.out.println(mataKuliah[4]);
+    nilaiMahasiswa[4][0] = nilaiMahasiswa[0][0] * bobot [0] +
+            nilaiMahasiswa[1][0] * bobot [1] +
+            nilaiMahasiswa[2][0] * bobot [2] +
+            nilaiMahasiswa[3][0] * bobot [3];
+
+    System.out.println("Nilai si mahasiswa 1 :"+nilaiMahasiswa[4][0]);
+
+    nilaiMahasiswa[0][1] = 80;
+    nilaiMahasiswa[1][1] = 90;
+    nilaiMahasiswa[2][1] = 75;
+    nilaiMahasiswa[3][1] = 70;
+
+    nilaiMahasiswa[4][1] = 0;
+    for (int i=0 ; i<=3 ; i++) {
+        nilaiMahasiswa[4][1] += nilaiMahasiswa[i][1] * bobot[i];
+    }
+
+    System.out.println("Nilai si mahasiswa 2 :"+nilaiMahasiswa[4][1]);
+
+    double jumlahNilaiAkhir = 0;
+    for (int i=0 ; i<=3 ; i++) {
+        jumlahNilaiAkhir += nilaiMahasiswa[4][i];
+    }
+    System.out.println(jumlahNilaiAkhir);
     }
 }
